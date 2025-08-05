1f94e93842d8c23699a2bd17e8bd293ce58de6f0
sanjog <sanjogsingh.p@gmail.com>
2025-08-05 13:53:36 +0530

    timer

--- src/ed2/Nb.jsx
+++ src/ed2/Nb.jsx
@@ -45,7 +45,7 @@ const Navbar = ({ activeTab, setActiveTab, onTimeExpired, isInterviewEnded }) =>
   const startTimer = () => {
     if (!isInterviewEnded) {
       setIsRunning(true);
-      setTimeLeft(1 * 60); // Reset to 2 minutes for testing (change to 60 * 60 for production)
+      setTimeLeft(5 * 60); // Reset to 2 minutes for testing (change to 60 * 60 for production)
       hasExpired.current = false;
     }
   };
